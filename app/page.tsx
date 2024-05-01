614aa108fa9004f024fc7af8a9c8331218f75333
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-05-01 18:47:14 +0300

    add todo table

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
+import { getTodoListAction } from "@/actions/todo.action";
 import AddTodoForm from "@/components/AddTodoForm";
+import TodoTable from "@/components/TodoTable";
 
-export default function Home() {
-  // const todos = await getTodoListAction();
+export default async function Home() {
+  const todos = await getTodoListAction();
 
   return (
     <main className="container">
-      {/* <ul>
+      <AddTodoForm />
+      <TodoTable />
+      <ul>
         {todos.map((todo) => (
           <li key={todo.id}>{todo.title}</li>
         ))}
-      </ul> */}
-      <AddTodoForm />
+      </ul>
     </main>
   );
 }
---
+++ components/TodoTable.tsx
@@ -0,0 +1,96 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "./ui/button";
+import { Pen, Trash } from "lucide-react";
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+];
+
+export default function TodoTable() {
+  return (
+    <Table>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Count</TableHead>
+          <TableHead>Title</TableHead>
+          <TableHead>Completed</TableHead>
+          <TableHead className="text-right">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {invoices.map((invoice, idx) => (
+          <TableRow key={invoice.invoice}>
+            <TableCell className="font-medium">{idx + 1}</TableCell>
+            <TableCell>Title</TableCell>
+            <TableCell>Completed</TableCell>
+            <TableCell className="flex items-center justify-end gap-2">
+              <Button size={"icon"}>
+                <Pen size={16} />
+              </Button>
+              <Button size={"icon"} variant={"destructive"}>
+                <Trash size={16} />
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell colSpan={3}>Total</TableCell>
+          <TableCell className="text-right">{invoices.length}</TableCell>
+        </TableRow>
+      </TableFooter>
+    </Table>
+  );
+}
